9d6d0a2e00b35c68a93f803ac5c6a40a2e4b4860
lakshraina2 <lakshraina2@gmail.com>
2026-05-28 23:17:32 +0530

    variableScope

---
+++ variable scope/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    static int x = 3;
+
+    public static void main(String[] args){
+
+        int x = 1;
+        //System.out.println(x);
+        method();
+
+    }
+    static void method(){
+        //int x = 2;
+        System.out.println(x);
+
+    }
+}
